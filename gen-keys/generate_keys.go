758e8b8047c03c6b7e0c8196b7cc442d43848872
Vladimir Sedlacek <vlada.sedlacek@gmail.com>
2019-03-07 15:40:24 +0100

    Clean up

--- gen-keys/generate_keys.go
+++ gen-keys/generate_keys.go
@@ -1,4 +1,3 @@
-
 /*
  * Genarate rsa keys.
  */
@@ -9,23 +8,23 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
-	"os"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
 
 	// If the file doesn't exist, create it, or append to the file
-    f, err := os.OpenFile("rsa2048.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
+	f, err := os.OpenFile("rsa2048.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Write header
-    if _, err := f.Write([]byte("id;n;e;d;p;q;t1\n")); err != nil {
-        log.Fatal(err)
-    }
+	// Write header
+	if _, err := f.Write([]byte("id;n;e;d;p;q;t1\n")); err != nil {
+		log.Fatal(err)
+	}
 
 	reader := rand.Reader
 	bitSize := 2048
@@ -35,7 +34,6 @@ func main() {
 		start := time.Now()
 		key, rsaerr := rsa.GenerateKey(reader, bitSize)
 		end := time.Now()
-		// t1 = end - start
 		t1 := end.Sub(start)
 
 		checkError(rsaerr)
@@ -48,17 +46,16 @@ func main() {
 		var p = key.Primes[0]
 		var q = key.Primes[1]
 
-	    // Write keys
-	    if _, err := f.Write([]byte(fmt.Sprintf("%d;%x;%x;%x;%x;%x;%d;\n", id,n,e,d,p,q,t1))); err != nil {
-	        log.Fatal(err)
-	    }
-	    
+		// Write keys
+		if _, err := f.Write([]byte(fmt.Sprintf("%d;%x;%x;%x;%x;%x;%d;\n", id, n, e, d, p, q, t1))); err != nil {
+			log.Fatal(err)
+		}
 
 	}
 
 	if err := f.Close(); err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 
 }
 
