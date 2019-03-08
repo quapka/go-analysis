d1b3f04fbc3c24c8572e938ce6e2fee6df078591
quapka <quapka@gmail.com>
2019-03-08 20:47:30 +0100

    #4 Use argparse for cmd line argument parsing

--- gen-keys/generate_keys.go
+++ gen-keys/generate_keys.go
@@ -8,34 +8,50 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
+	"github.com/akamensky/argparse"
 	"io"
 	"log"
 	"os"
-	"strconv"
 	"time"
 )
 
+const typeRSA string = "rsa"
+const typeECC string = "ecc"
+
 func main() {
+	// I'm having troubles with declaration/definition and scope in Go,
+	// therefore I'm declaring err here, so later I can use
+	// if <var>, err = [...] <-- no colon here
+	// that is, definition without declaration
 	var err error
+	// os.Args[0] will be the name of the executable and therefore give
+	// more meaningful help message
+	parser := argparse.NewParser(os.Args[0], "Generates RSA and ECC key for further analysis")
 
-	algorithm := os.Args[1]
-	arg2 := os.Args[2]
-	arg3 := os.Args[3]
+	rsaCmd := parser.NewCommand(typeRSA, "Generate a RSA private key")
+	// bit-size is valid option only for RSA
+	bitSize := rsaCmd.Int("b", "bit-size", &argparse.Options{Required: true, Help: "Specify the bit size of the private key"})
 
-	bitSize, err := strconv.Atoi(arg2)
+	eccCmd := parser.NewCommand(typeECC, "Generate a ECC private key")
+
+	keyCount := parser.Int("c", "key-count", &argparse.Options{Required: true, Help: "Specify the number of keys to be generated"})
+
+	err = parser.Parse(os.Args)
 	if err != nil {
-		fmt.Printf("bitSize must be an integer and not %T!\n", arg2)
-		os.Exit(1)
-	}
-
-	keyCount, err := strconv.Atoi(arg3)
-	if err != nil {
-		fmt.Printf("keyCount must be an integer and not %T!\n", arg3)
-		os.Exit(1)
+		fmt.Print(parser.Usage(err))
+		return
 	}
 
 	// If the file doesn't exist, create it, or append to the file
-	filename := fmt.Sprintf("%s%d.csv", algorithm, bitSize)
+	filename := ""
+	if rsaCmd.Happened() {
+		filename = fmt.Sprintf("%s_%db_%d.csv", typeRSA, *bitSize, *keyCount)
+	} else if eccCmd.Happened() {
+		filename = fmt.Sprintf("%s_%d.csv", typeECC, *keyCount)
+	} else {
+		log.Fatal("Please, specify a 'rsa' or 'ecc' key type.")
+	}
+
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -50,20 +66,22 @@ func main() {
 	// but just call it, when it is needed
 	reader := rand.Reader
 
-	for id := 0; id < keyCount; id++ {
+	for id := 0; id < *keyCount; id++ {
 		var dataRow string = ""
 		var data string = ""
 
 		idPrefix := fmt.Sprintf("%d;", id)
 
-		if algorithm == "rsa" {
-			data, err = getRSAData(reader, bitSize)
+		if rsaCmd.Happened() {
+			data, err = getRSAData(reader, *bitSize)
 			if err != nil {
 				// fmt.Printf("Got errorneous data: %s\n", data)
 				log.Fatal(err)
 			}
-		} else if algorithm == "ecc" {
+		} else if eccCmd.Happened() {
 			log.Fatal("Generating ECC keys is not implemented yet.")
+		} else {
+			log.Fatal("Please, specify a 'rsa' or 'ecc' key type.")
 		}
 
 		dataRow = idPrefix + data + "\n"
