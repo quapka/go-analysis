4ef06eefd888a107fadbe061cc01bc853bf78cec
x451363 <451363@mail.muni.cz>
2019-03-10 14:08:05 +0100

    Add heatmap graphs. Refactor existing functions.

--- graphs/generate_graphs.py
+++ graphs/generate_graphs.py
@@ -18,18 +18,43 @@ def plot_keygen_time_for_generated_keys(data):
     plt.xticks(np.arange(0, num_of_rows, num_of_steps), data.index.values[0::num_of_steps])
 
 
-def plot_histogram_of_msb_of_prime_p(data):
-    data['p']\
-        .apply(lambda x: x[:2])\
-        .apply(lambda x: int(x, 16))\
+# Returns byte in hex_string on byte_pos position. MSB has byte_pos = 0, LSB has byte_pos = -1.
+def get_byte(hex_string, byte_pos):
+    return [hex_string[i:i + 2] for i in range(0, len(hex_string), 2)][byte_pos]
+
+
+def plot_byte_histogram(data, column_name, byte_pos):
+    data[column_name] \
+        .apply(lambda x: int(get_byte(x, byte_pos), 16)) \
         .plot.hist(bins=256)
 
 
-def plot_histogram_of_lsb_of_prime_p(data):
-    data['p']\
-        .apply(lambda x: x[-2:])\
-        .apply(lambda x: int(x, 16))\
-        .plot.hist(bins=256)
+def plot_msb_histogram(data, column_name):
+    plot_byte_histogram(data, column_name, 0)
+
+
+def plot_lsb_histogram(data, column_name):
+    plot_byte_histogram(data, column_name, -1)
+
+
+def plot_byte_heatmap(data, column_name, byte_pos):
+    index = data['t1'].apply(lambda x: x // 1000000)
+    columns = data[column_name].apply(lambda x: int(get_byte(x, byte_pos), 16))
+    cross_table = pd.crosstab(
+        index=index,
+        columns=columns,
+        dropna=False
+    ).transpose().reindex(np.arange(0, 256)).transpose().fillna(0)
+
+    plt.pcolor(cross_table)
+
+
+def plot_msb_heatmap(data, column_name):
+    plot_byte_heatmap(data, column_name, 0)
+
+
+def plot_lsb_heatmap(data, column_name):
+    plot_byte_heatmap(data, column_name, -1)
 
 
 def generate_and_save_graphs(data, file_name_prefix, limit):
@@ -44,12 +69,20 @@ def generate_and_save_graphs(data, file_name_prefix, limit):
         plt.savefig(file_name_prefix + "_keygen_times.png")
         plt.clf()
 
-    plot_histogram_of_msb_of_prime_p(sampleData)
+    plot_msb_histogram(sampleData, 'p')
     plt.savefig(file_name_prefix + "_hist_msb_p.png")
     plt.clf()
 
-    plot_histogram_of_lsb_of_prime_p(sampleData)
+    plot_lsb_histogram(sampleData, 'p')
     plt.savefig(file_name_prefix + "_hist_lsb_p.png")
+    plt.clf()
+
+    plot_msb_heatmap(sampleData, 'p')
+    plt.savefig(file_name_prefix + "_heatmap_msb_p.png")
+    plt.clf()
+
+    plot_lsb_heatmap(sampleData, 'p')
+    plt.savefig(file_name_prefix + "_heatmap_lsb_p.png")
     plt.clf()
 
 
