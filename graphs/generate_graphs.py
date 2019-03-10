8d57ec2186048fde9938f0e7e01e3ec9c8eb687d
x451363 <451363@mail.muni.cz>
2019-03-10 08:25:01 +0100

    Add saving to files.

--- graphs/generate_graphs.py
+++ graphs/generate_graphs.py
@@ -3,42 +3,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def show_histogram_of_keygen_times(data):
+def plot_histogram_of_keygen_times(data):
     data['t1'].plot.hist()
-    plt.show()
 
 
-def show_keygen_time_for_generated_keys(data):
+# plotting of large amount of data takes a lot of time and memory
+def plot_keygen_time_for_generated_keys(data):
     data.plot.bar(x='id', y='t1', width=1)
 
     num_of_rows = len(data.index)
     num_of_steps = num_of_rows // 10
     plt.xticks(np.arange(0, num_of_rows, num_of_steps), data.index.values[0::num_of_steps])
 
-    plt.show()
 
-
-def show_histogram_of_msb_of_prime_p(data):
+def plot_histogram_of_msb_of_prime_p(data):
     data['p']\
         .apply(lambda x: x[:2])\
         .apply(lambda x: int(x, 16))\
         .plot.hist(bins=256)
 
-    plt.show()
 
-
-def show_histogram_of_lsb_of_prime_p(data):
+def plot_histogram_of_lsb_of_prime_p(data):
     data['p']\
         .apply(lambda x: x[-2:])\
         .apply(lambda x: int(x, 16))\
         .plot.hist(bins=256)
 
-    plt.show()
+
+def generate_and_save_graphs(data, file_name_prefix, small_data_only):
+    data_size = len(data.index)
+
+    plot_histogram_of_keygen_times(sampleData)
+    plt.savefig(file_name_prefix + "_hist_keygen_times.png")
+    plt.clf()
+
+    if data_size <= 10000 or not small_data_only:
+        plot_keygen_time_for_generated_keys(sampleData)
+        plt.savefig(file_name_prefix + "_keygen_times.png")
+        plt.clf()
+
+    plot_histogram_of_msb_of_prime_p(sampleData)
+    plt.savefig(file_name_prefix + "_hist_msb_p.png")
+    plt.clf()
+
+    plot_histogram_of_lsb_of_prime_p(sampleData)
+    plt.savefig(file_name_prefix + "_hist_lsb_p.png")
+    plt.clf()
 
 
-sampleData = pd.read_csv('../sample-data/rsa_512b_100.csv', delimiter=';')
+sampleData = pd.read_csv(
+    '../sample-data/rsa_512b_10000.csv',
+    delimiter=';',
+    low_memory=False,
+)
 
-show_histogram_of_keygen_times(sampleData)
-show_keygen_time_for_generated_keys(sampleData)
-show_histogram_of_msb_of_prime_p(sampleData)
-show_histogram_of_lsb_of_prime_p(sampleData)
+
+generate_and_save_graphs(sampleData, "rsa_512b_10000", True)
