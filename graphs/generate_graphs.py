b3337c197dbb11fa415503ead4465c1fac548c7d
x451363 <451363@mail.muni.cz>
2019-03-10 09:32:39 +0100

    Add argparse.

--- graphs/generate_graphs.py
+++ graphs/generate_graphs.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+import os
 
 
 def plot_histogram_of_keygen_times(data):
@@ -30,14 +32,14 @@ def plot_histogram_of_lsb_of_prime_p(data):
         .plot.hist(bins=256)
 
 
-def generate_and_save_graphs(data, file_name_prefix, small_data_only):
+def generate_and_save_graphs(data, file_name_prefix, limit):
     data_size = len(data.index)
 
     plot_histogram_of_keygen_times(sampleData)
     plt.savefig(file_name_prefix + "_hist_keygen_times.png")
     plt.clf()
 
-    if data_size <= 10000 or not small_data_only:
+    if data_size <= limit:
         plot_keygen_time_for_generated_keys(sampleData)
         plt.savefig(file_name_prefix + "_keygen_times.png")
         plt.clf()
@@ -51,11 +53,24 @@ def generate_and_save_graphs(data, file_name_prefix, small_data_only):
     plt.clf()
 
 
+# parse arguments
+parser = argparse.ArgumentParser(description="Graphs generator.")
+parser.add_argument('file', type=argparse.FileType('r'), nargs=1)
+parser.add_argument('-n', default='', metavar='name_prefix',
+                    help='Prefix of the filenames of the generated graphs. Default is the data filename.')
+parser.add_argument('-l', metavar='limit', type=int, default=10000,
+                    help='Some graphs can take only small amount of data. '
+                         'This specifies the upper limit. The default is 10000.')
+args = parser.parse_args()
+
+# load data from csv
 sampleData = pd.read_csv(
-    '../sample-data/rsa_512b_10000.csv',
+    args.file[0],
     delimiter=';',
     low_memory=False,
 )
 
-
-generate_and_save_graphs(sampleData, "rsa_512b_10000", True)
+# generate graphs
+file_basename = os.path.splitext(os.path.basename(args.file[0].name))[0]
+prefix = file_basename if args.n == '' else args.n
+generate_and_save_graphs(sampleData, prefix, args.l)
