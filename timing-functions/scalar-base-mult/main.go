08b3693eba87fe29ff9e36abb4a412252417aca0
x451363 <451363@mail.muni.cz>
2019-03-21 18:31:48 +0100

    Implement loading input data from file.

--- timing-functions/scalar-base-mult/main.go
+++ timing-functions/scalar-base-mult/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"crypto/elliptic"
+	"encoding/csv"
+	"errors"
 	"fmt"
 	"github.com/akamensky/argparse"
+	"io/ioutil"
 	"log"
-	"math"
+	"math/big"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -14,14 +18,19 @@ func main() {
 	// setup parser
 	parser := argparse.NewParser(os.Args[0], "Generate time measurements for ScalarBaseMult function on p256.")
 	numIterationArg := parser.Int("n", "num-iteration", &argparse.Options{Required: true, Help: "Specify the number of iteration for each input"})
-	_ = parser.String("f", "filename", &argparse.Options{Required: true, Help: "[NOT IMPLEMENTED YET] The path to the file that contains the inputs."})
+	inputFileName := parser.String("f", "filename", &argparse.Options{Required: true, Help: "The path to the file that contains the inputs."})
 
 	// parse argument
-	err := parser.Parse(os.Args)
 	// if parsing failed print usage and return
-	if err != nil {
+	if err := parser.Parse(os.Args); err != nil {
 		fmt.Print(parser.Usage(err))
 		return
+	}
+
+	// load input data as [][]string
+	data, err := loadInputData(*inputFileName)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// open outfile
@@ -29,23 +38,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	p256 := elliptic.P256()
+
 	// write column headers
-	for j := 0; j < 256; j++ {
-		if _, err := outFile.WriteString(fmt.Sprintf("%d;", j)); err != nil {
+	for _, row := range data {
+		inputLabel := row[0]
+		// write input label (column header)
+		if _, err := outFile.WriteString(inputLabel + ";"); err != nil {
 			log.Fatal(err)
 		}
 	}
+
 	// write endline
 	if _, err := outFile.WriteString("\n"); err != nil {
 		log.Fatal(err)
 	}
 
-	p256 := elliptic.P256()
-
+	// iterate over rows
 	for i := 0; i < *numIterationArg; i++ {
-		for j := 0; j < 256; j++ {
-			// measured function input parameters
-			scalar := []byte{byte(math.Pow(2, float64(j)))}
+		// iterate over columns
+		for _, row := range data {
+			// input for the measured function
+			scalar, err := stringToIntBytes(row[1])
+			if err != nil {
+				log.Fatal(err)
+			}
 
 			// timing
 			start := time.Now()
@@ -71,4 +89,40 @@ func main() {
 	if err := outFile.Close(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Reads file as csv separated with semicolon.
+func loadInputData(fileName string) ([][]string, error) {
+
+	// read whole file into data
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	// new csv reader, separator = ;
+	r := csv.NewReader(strings.NewReader(string(data)))
+	r.Comma = ';'
+
+	// read all
+	result, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+// Converts a number represented as decimal string into bytes.
+// Error if the string does not represent a decimal number.
+func stringToIntBytes(input string) ([]byte, error) {
+
+	value := new(big.Int)
+
+	value, ok := value.SetString(input, 10)
+	if !ok {
+		return nil, errors.New("Cannot convert \"" + input + "\" into a number.")
+	}
+
+	return value.Bytes(), nil
 }
