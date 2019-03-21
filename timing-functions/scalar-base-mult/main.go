d60311db152ac38e806b36136eaba836b35f195c
x451363 <451363@mail.muni.cz>
2019-03-21 17:01:15 +0100

    Refactor.

--- timing-functions/scalar-base-mult/main.go
+++ timing-functions/scalar-base-mult/main.go
@@ -11,47 +11,63 @@ import (
 )
 
 func main() {
-	parser := argparse.NewParser(os.Args[0], "Generate time measurements for ScalarBaseMult function")
-	numIteration := parser.Int("n", "num-iteration", &argparse.Options{Required: true, Help: "Specify the number of iteration for each input"})
+	// setup parser
+	parser := argparse.NewParser(os.Args[0], "Generate time measurements for ScalarBaseMult function on p256.")
+	numIterationArg := parser.Int("n", "num-iteration", &argparse.Options{Required: true, Help: "Specify the number of iteration for each input"})
 	_ = parser.String("f", "filename", &argparse.Options{Required: true, Help: "[NOT IMPLEMENTED YET] The path to the file that contains the inputs."})
 
+	// parse argument
 	err := parser.Parse(os.Args)
+	// if parsing failed print usage and return
 	if err != nil {
 		fmt.Print(parser.Usage(err))
 		return
 	}
 
-	p256 := elliptic.P256()
+	// open outfile
 	outFile, err := os.OpenFile("data.csv", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// write column headers
 	for j := 0; j < 256; j++ {
-		if _, err := outFile.Write([]byte(fmt.Sprintf("%d;", j))); err != nil {
+		if _, err := outFile.WriteString(fmt.Sprintf("%d;", j)); err != nil {
 			log.Fatal(err)
 		}
 	}
-	if _, err := outFile.Write([]byte("\n")); err != nil {
+	// write endline
+	if _, err := outFile.WriteString("\n"); err != nil {
 		log.Fatal(err)
 	}
-	for i := 0; i < *numIteration; i++ {
+
+	p256 := elliptic.P256()
+
+	for i := 0; i < *numIterationArg; i++ {
 		for j := 0; j < 256; j++ {
+			// measured function input parameters
 			scalar := []byte{byte(math.Pow(2, float64(j)))}
+
+			// timing
 			start := time.Now()
-			_, _ = p256.ScalarBaseMult(scalar)
+			_, _ = p256.ScalarBaseMult(scalar) // measured function
 			end := time.Now()
 			elapsed := end.Sub(start)
 			t1 := elapsed.Nanoseconds()
 
-			if _, err := outFile.Write([]byte(fmt.Sprintf("%d;", t1))); err != nil {
+			// write t1
+			if _, err := outFile.WriteString(fmt.Sprintf("%d;", t1)); err != nil {
 				log.Fatal(err)
 			}
 
 		}
-		if _, err := outFile.Write([]byte("\n")); err != nil {
+
+		// write endline
+		if _, err := outFile.WriteString("\n"); err != nil {
 			log.Fatal(err)
 		}
 	}
+
+	// close outfile
 	if err := outFile.Close(); err != nil {
 		log.Fatal(err)
 	}
