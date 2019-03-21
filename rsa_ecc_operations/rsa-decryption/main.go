0685310d514c795595a21e2f8e68274b847cd733
quapka <quapka@gmail.com>
2019-03-21 15:14:40 +0100

    Add code for profiling functions

--- rsa_ecc_operations/rsa-decryption/main.go
+++ rsa_ecc_operations/rsa-decryption/main.go
@@ -9,9 +9,25 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/hex"
+	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	key := make([]byte, 32)
 	rng := rand.Reader
 	rsaCiphertext, _ := hex.DecodeString("aabbccddeeff")
