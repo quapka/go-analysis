2498315e33e6863503841f91f7526305324e4a99
x451363 <451363@mail.muni.cz>
2019-04-03 11:40:43 +0200

    Implement random point by weight.

--- timing-functions/input-data-generators/generate.py
+++ timing-functions/input-data-generators/generate.py
@@ -1,5 +1,24 @@
 import csv
 import random
+import modular
+
+
+def generate_random_point_by_weight(weight):
+    a = -3
+    b = 41058363725152142129326129780047268409114441015993725554835256314039467401291
+    p = 2 ** 256 - 2 ** 224 + 2 ** 192 + 2 ** 96 - 1
+
+    while True:
+        x = generate_random_by_weight(256, weight)
+        y_2 = x ** 3 + a * x + b
+        y = modular.modular_sqrt(y_2, p)
+
+        if y != 0:
+            return x, y
+
+
+def generate_random_by_weight_range(bit_size, min_weight, max_weight):
+    return generate_random_by_weight(bit_size, random.randrange(min_weight, max_weight + 1))
 
 
 def generate_random_by_weight(bit_size, weight):
@@ -14,7 +33,7 @@ def generate_random_by_weight_to_file(filename, num_of_values, bit_size, weight)
         for i in range(0, num_of_values):
             value = generate_random_by_weight(bit_size, weight)
             value_string = format(value, 'X')
-            label = "col_rnd_" + str(i)
+            label = "col_rnd_weight_" + weight + "_" + str(i)
 
             writer = csv.writer(csvfile, delimiter=';')
             writer.writerow([label, value_string])
@@ -29,7 +48,3 @@ def generate_random_to_file(filename, num_of_values, min_value, max_value):
 
             writer = csv.writer(csvfile, delimiter=';')
             writer.writerow([label, value_string])
-
-
-# generate_random_to_file("test.csv", 10, 1, 2**256-432420386565659656852420866394968145599)
-generate_random_by_weight_to_file("test.csv", 10, 256, 200)
