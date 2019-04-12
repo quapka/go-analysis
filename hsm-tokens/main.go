8b0199bb32885a4150bc0d99976dac4bc6ffc84e
quapka <quapka@gmail.com>
2019-04-12 13:52:43 +0200

    Update the PKCS#11-hello world with RSA key generation

--- hsm-tokens/main.go
+++ hsm-tokens/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/miekg/pkcs11"
+	"log"
 )
 
 func main() {
@@ -32,14 +33,46 @@ func main() {
 	}
 	defer p.Logout(session)
 
-	p.DigestInit(session, []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_SHA_1, nil)})
-	hash, err := p.Digest(session, []byte("this is a string"))
-	if err != nil {
-		panic(err)
+	tokenPersistent := true
+	tokenLabel := true
+	publicKeyTemplate := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
+		pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_RSA),
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, tokenPersistent),
+		pkcs11.NewAttribute(pkcs11.CKA_VERIFY, true),
+		pkcs11.NewAttribute(pkcs11.CKA_PUBLIC_EXPONENT, []byte{1, 0, 0, 0, 1}),
+		pkcs11.NewAttribute(pkcs11.CKA_MODULUS_BITS, 2048),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, tokenLabel),
 	}
+	privateKeyTemplate := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, tokenPersistent),
+		pkcs11.NewAttribute(pkcs11.CKA_SIGN, true),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, tokenLabel),
+		pkcs11.NewAttribute(pkcs11.CKA_SENSITIVE, true),
+		pkcs11.NewAttribute(pkcs11.CKA_EXTRACTABLE, true),
+	}
+	pbk, pvk, e := p.GenerateKeyPair(session,
+		[]*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_RSA_PKCS_KEY_PAIR_GEN, nil)},
+		publicKeyTemplate, privateKeyTemplate)
+	if e != nil {
+		log.Fatal("failed to generate keypair: %s\n", e)
+	}
+	fmt.Println(fmt.Sprintf("%x", pbk))
+	fmt.Println(fmt.Sprintf("%x", pvk))
+	// p.GenerateKeyPair(
+	//     session,
+	//     []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_RSA_PKCS_KEY_PAIR_GEN, nil)},
+	//     []*pkcs11.Attribute)
+	fmt.Println("Print")
+	// key, err := p.
+	// p.DigestInit(session, []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_SHA_1, nil)})
+	// hash, err := p.Digest(session, []byte("this is a string"))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	for _, d := range hash {
-		fmt.Printf("%x", d)
-	}
-	fmt.Println()
+	// for _, d := range hash {
+	// 	fmt.Printf("%x", d)
+	// }
+	// fmt.Println()
 }
